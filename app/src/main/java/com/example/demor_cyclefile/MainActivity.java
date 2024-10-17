fcedc3e8c246842e6509e92e912178feae0109e6
nemngonnhi <102734863+nemngonnhi@users.noreply.github.com>
2024-10-18 01:01:37 +0700

    b2

---
+++ app/src/main/java/com/example/demor_cyclefile/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.demor_cyclefile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends Activity {
+    Button btncall;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "CR424 - onCreat()", Toast.LENGTH_SHORT).show();
+        btncall = findViewById(R.id.btncall);
+        btncall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent1 = new Intent(MainActivity.this, subactivity.class);
+                startActivity(intent1);
+            }
+        });
+    }}
